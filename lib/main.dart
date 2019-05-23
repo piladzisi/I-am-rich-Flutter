b6daf2a75bf72b8aaa558903e087e312344cdd81
piladzisi <piladzisi@gmail.com>
2019-05-23 19:46:00 +0300

    image (can't see on android)

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,18 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Center(
-        child: Text("Hello world"),
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          title: Center(child: Text('I am Rich')),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: Center(
+          child: Image(
+            image:
+                NetworkImage('https://www.gia.edu/images/polished-diamond.png'),
+          ),
+        ),
       ),
     ),
   );
