0df50ce95948bc352a9a6da5108353188d6751f4
piladzisi <piladzisi@gmail.com>
2019-05-23 17:24:51 +0300

    testing

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,12 @@
-// Flutter code sample for material.Scaffold.1
-
-// This example shows a [Scaffold] with an [AppBar], a [BottomAppBar] and a
-// [FloatingActionButton]. The [body] is a [Text] placed in a [Center] in order
-// to center the text within the [Scaffold] and the [FloatingActionButton] is
-// centered and docked within the [BottomAppBar] using
-// [FloatingActionButtonLocation.centerDocked]. The [FloatingActionButton] is
-// connected to a callback that increments a counter.
-
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(home: Text("Hello World1")));
+//The main function is the starting point for all Flutter apps
+void main() {
+  runApp(
+    MaterialApp(
+      home: Center(
+        child: Text("Hello world"),
+      ),
+    ),
+  );
+}
